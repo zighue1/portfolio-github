5ce4e1513a92338f7cbb9279f660eb69b3e202a1
zighue1 <93688115+zighue1@users.noreply.github.com>
2021-12-17 21:13:28 -0300

    Exercicio1

---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,8 @@
+export const Summary = () => {
+    return (
+        <>
+            <h1>Eu sou o Zighue2</h1>
+            <img alt="imagem de perfil" src={"https://github.com/zighue1.png"}></img>
+        </> 
+    )
+}
