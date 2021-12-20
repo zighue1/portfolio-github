bdbf4b2e973b16057460a4c70bfd65d4c5d9a9e7
zighue1 <93688115+zighue1@users.noreply.github.com>
2021-12-20 19:24:22 -0300

    Exercicio 5 PropTypes

--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -1,9 +1,17 @@
-export const Repository = () => {
+import PropTypes from 'prop-types';
+
+export const Repository = ({titulo, descricao, destacar}) => {
     return (
         <>
-            <h3>Terceira Imagem</h3>
+            <h3>{titulo}</h3>
             <span>projeto em destaque!</span>
-            <p>asdasdasd askjdsajkd jasdjasjd jasdjasd kasdkasd kasdkjasdk</p>
+            <p>{descricao}</p>
         </> 
     )
+}
+
+Repository.propTypes = {
+    titulo : PropTypes.string.isRequired,
+    descricao : PropTypes.string,
+    destacar : PropTypes.bool
 }
